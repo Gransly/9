37a9417714de1bd315b7c7329b36bf61c47a9010
Gransly <bob031299@gmail.com>
2019-12-01 21:58:11 +1000

    Stamina feature (jump/sprint)

--- Lesson9/Assets/Script/PlayerControls.cs
+++ Lesson9/Assets/Script/PlayerControls.cs
@@ -2,11 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerControls : MonoBehaviour
 {
-
+    private HUD hud;
+    
     public float movespeed;
+    public float walkspeed;
+    public float runspeed;
+
+    public float jumpStamiaMin;
+    public float staminaMin ;
+    private float stamina = 100f;
+    private float timerToRegenStamina = 5f;
+    private float timeToRegenStamina = 1f;
+    private float staminaRegenSpeed = 15f;
+    
     public Transform camTransform;
     public float jumpforce;
     
@@ -16,22 +28,36 @@ public class PlayerControls : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        
+        hud = GameObject.FindGameObjectWithTag("Canvas").GetComponent<HUD>();
         playerTransform = GetComponent<Transform>();
     }
 
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        if (other.CompareTag())
-//        {
-//            
-//        }
-//    }
-
-
     void Update()
     {
-        
+        if (stamina > 0  && Input.GetKey(KeyCode.LeftShift) && ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)) ))
+        {
+            movespeed = runspeed;
+            stamina -= staminaMin * Time.deltaTime;
+            hud.staminaBar.fillAmount = stamina / 100;
+            stamina = Mathf.Clamp(stamina, 0, 100);
+            timerToRegenStamina = 0f;
+        }
+        else
+        {
+            movespeed = walkspeed;
+            if (timerToRegenStamina < timeToRegenStamina)
+            {
+                timerToRegenStamina += Time.deltaTime; 
+            }
+            else if (timerToRegenStamina >= timeToRegenStamina &&  stamina < 100f)
+            {
+                Regeniration();  
+            }
+            
+                
+            
+            
+        }
         
         if (Input.GetKey(KeyCode.W))
         {
@@ -51,9 +77,20 @@ public class PlayerControls : MonoBehaviour
             rb.AddForce(camTransform.right* movespeed * Time.fixedDeltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && stamina > 30f)
         {
+            stamina -= jumpStamiaMin * Time.deltaTime;
+            hud.staminaBar.fillAmount = stamina / 100;
+            stamina = Mathf.Clamp(stamina, 0, 100);
+            timerToRegenStamina = 0f;
             rb.AddForce(Vector3.up* jumpforce * Time.fixedDeltaTime);
         }
+    }
+
+    private void Regeniration()
+    {
+        stamina += staminaRegenSpeed * Time.deltaTime;
+        hud.staminaBar.fillAmount = stamina / 100;
+        stamina = Mathf.Clamp(stamina, 0, 100);
     }
 }
