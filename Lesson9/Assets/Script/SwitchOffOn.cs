a756b6c2da6d1ff5be069a669efd14751276e42b
Gransly <bob031299@gmail.com>
2019-12-22 16:03:32 +1000

    Off/On script with bag

---
+++ Lesson9/Assets/Script/SwitchOffOn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchOffOn : MonoBehaviour
+{
+    public BoxCollider colliderToOff;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !colliderToOff.enabled)  //TODO одновременно включается и отключается
+        {
+            Debug.Log("on");
+            colliderToOff.enabled = true;
+        }
+        
+        if (other.CompareTag("Player") && colliderToOff.enabled)
+        {
+            Debug.Log("off");
+            colliderToOff.enabled = false;
+        }
+        
+        
+    }
+}
